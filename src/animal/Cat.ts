718c7f5782477e25cb1e65f34778d9bc780599cc
unknown <tomerpacific@gmail.com>
2020-10-03 11:50:13 +0300

    feature/cat-model created the model class for Cat

---
+++ src/animal/Cat.ts
@@ -0,0 +1,17 @@
+import {Animal} from "./Animal";
+
+export class Cat extends Animal {
+
+    private constructor(height: number, runningSpeedPerKm: number) {
+        super(height, runningSpeedPerKm);
+    }
+
+    static create(height: number, runningSpeedPerKm: number): Cat {
+        return new Cat(height, runningSpeedPerKm);
+    }
+
+    communicate(): void {
+        console.error('Meow meow');
+    }
+
+}
