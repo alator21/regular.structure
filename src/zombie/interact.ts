57dff937a206fceaa2d0fa854c0693b2c1c878fc
alator21 <botazz133@gmail.com>
2025-10-03 19:49:26 +0300

    zombie animation

--- src/zombie/interact.ts
+++ src/zombie/interact.ts
@@ -1,5 +1,23 @@
-import {AngryZombie} from "./AngryZombie";
+import { AngryZombie } from "./AngryZombie";
+import { FriendlyZombie } from "./FriendlyZombie";
+import { Animator } from "../animation/Animator";
+import { ZombieApocalypseAnimation } from "./ZombieApocalypseAnimation";
 
-const angryZombie: AngryZombie = AngryZombie.create("EGYPT");
-angryZombie.printDetails();
-angryZombie.interactWithHuman();
+async function main() {
+    console.log("=== Zombie Apocalypse Story ===\n");
+    await Animator.play(ZombieApocalypseAnimation.create(), 1);
+
+    console.log("\n=== Zombie Types ===");
+
+    console.log("\n--- Angry Zombie ---");
+    const angryZombie: AngryZombie = AngryZombie.create("EGYPT");
+    angryZombie.printDetails();
+    angryZombie.interactWithHuman();
+
+    console.log("\n--- Friendly Zombie ---");
+    const friendlyZombie: FriendlyZombie = FriendlyZombie.create("TOKYO");
+    friendlyZombie.printDetails();
+    friendlyZombie.interactWithHuman();
+}
+
+main();
